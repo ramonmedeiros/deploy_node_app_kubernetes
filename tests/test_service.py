40bd8e8afe6f66e26e84be2637ebadd3c3c70280
Ramon Medeiros <ramonm@spotify.com>
2019-01-29 17:04:53 +0100

    small fix on tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -6,10 +6,11 @@ URL = "http://localhost:30000"
 class TestService(TestCase):
 
     def test_service_up(self):
-        with requests.get(URL) as connection:
-            self.assertEquals(connection.status_code, 200)
+        connection = requests.get(URL)
+        self.assertEquals(connection.status_code, 200)
+        connection.close()
 
     def test_title(self):
-        with requests.get(URL) as connection:
-            self.assertTrue("<title>Redux Todos Example</title>" in connection.content.decode())
-
+        connection = requests.get(URL)
+        self.assertTrue("<title>Redux Todos Example</title>" in connection.content.decode())
+        connection.close()
